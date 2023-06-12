01940182d5392d6520d87ec3ee0e0c55753d6a63
Nico Gutierrez <nicolasmgutierrezf@gmail.com>
2023-06-12 05:29:40 +0000

    “nicode”

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Lista de palabras en un array
+  var first = ["mi ", "un "];
+  var noun = ["perro ", "compañero "];
+  var action = ["me mojó ", "me escondió ", "me rompió ", "me ensució "];
+  var possetion = [
+    "los zapatos ",
+    "la chaqueta ",
+    "el chaleco ",
+    "el pantalón "
+  ];
+  var where = [
+    "justo antes de salir de casa",
+    "en el parque",
+    "justo cuando venia llegando",
+    "cuando iba camino a la escuela"
+  ];
+
+  // Declarando variables aleatorias
+  var rdm1 = Math.floor(Math.random() * first.length);
+  var rdm2 = Math.floor(Math.random() * noun.length);
+  var rdm3 = Math.floor(Math.random() * action.length);
+  var rdm4 = Math.floor(Math.random() * possetion.length);
+  var rdm5 = Math.floor(Math.random() * where.length);
+
+  //Creando una frase (la excusa)
+  document.querySelector("#laexcusa").innerHTML =
+    first[rdm1] + noun[rdm2] + action[rdm3] + possetion[rdm4] + where[rdm5];
 };
